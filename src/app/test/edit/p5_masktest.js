077c1e859fe8e63b8f87782799494d39b2b82c68
2k2e2n <98691163+2k2e2n@users.noreply.github.com>
2025-06-14 23:03:33 +0900

    Update p5_masktest.js

--- src/app/test/edit/p5_masktest.js
+++ src/app/test/edit/p5_masktest.js
@@ -1,77 +1,83 @@
-let images = []; // 画像オブジェクトの配列
 
-function preload() {
-  // 各画像とマスクを読み込む
-  let img1 = loadImage('3.jpg');
-  let mask1 = loadImage('mask1.png');
+        /////////////////////////////////// ここからp5.jsを貼り付け ///////////////////////////////////
 
-  let img2 = loadImage('2.jpg');
-  let mask2 = loadImage('mask2.png');
+        let images = []; // 画像オブジェクトの配列
 
-  images.push({ img: img1, mask: mask1, pos: [50, 50], dragging: false, offset: [0, 0] });
-  images.push({ img: img2, mask: mask2, pos: [200, 200], dragging: false, offset: [0, 0] });
-}
+        preload = () => {
+          // 各画像とマスクを読み込む
+          let img1 = loadImage('3.jpg');
+          let mask1 = loadImage('mask1.png');
+          let img2 = loadImage('2.jpg');
+          let mask2 = loadImage('mask2.png');
 
-function setup() {
-  createCanvas(500, 500);
+          images = [
+            { img: img1, mask: mask1, pos: [50, 50], dragging: false, offset: [0, 0] },
+            { img: img2, mask: mask2, pos: [50, 200], dragging: false, offset: [0, 0] }
+          ];
+        };
 
-  // 画像とマスクをリサイズ（キャンバスサイズに合わせる）
-  for (let obj of images) {
-    obj.img.resize(width, height);
-    obj.mask.resize(width, height);
-  }
+        setup = () => {
+          createCanvas(500, 500);
 
-  imageMode(CORNER);
-  noLoop();
-}
+          // 画像とマスクをリサイズ（キャンバスサイズに合わせる）
+          for (let obj of images) {
+            obj.img.resize(width, height);
+            obj.mask.resize(width, height);
+          }
 
-function draw() {
-  background(0, 102, 153);
+          imageMode(CORNER);
+          noLoop();
+        };
 
-  for (let obj of images) {
-    let g = createGraphics(width, height);
-    g.image(obj.img, obj.pos[0], obj.pos[1]);
+        draw = () => {
+          background(0, 102, 153);
 
-    let maskedImg = g.get();
-    maskedImg.mask(obj.mask);
+          for (let obj of images) {
+            let g = createGraphics(width, height);
+            g.image(obj.img, obj.pos[0], obj.pos[1]);
 
-    image(maskedImg, 0, 0);
-  }
-}
+            let maskedImg = g.get();
+            maskedImg.mask(obj.mask);
 
-function mousePressed() {
-  for (let obj of images) {
-    let [x, y] = obj.pos;
-    let mx = mouseX - x;
-    let my = mouseY - y;
+            image(maskedImg, 0, 0);
+          }
+        };
 
-    // 座標が画像範囲外なら無視
-    if (mx < 0 || my < 0 || mx >= obj.img.width || my >= obj.img.height) continue;
+        mousePressed = () => {
+          for (let obj of images) {
+            let [x, y] = obj.pos;
+            let mx = mouseX - x;
+            let my = mouseY - y;
 
-    // マスクのアルファ値取得（RGBA）
-    let maskColor = obj.mask.get(mouseX, mouseY); // canvas全体にマスクがあるのでそのまま座標を使う
-    let alpha = maskColor[3];
+            // 座標が画像範囲外なら無視
+            if (mx < 0 || my < 0 || mx >= obj.img.width || my >= obj.img.height) continue;
 
-    // アルファが一定以上なら有効クリックと判定
-    if (alpha > 10) {
-      obj.dragging = true;
-      obj.offset = [mx, my];
-      break;
-    }
-  }
-}
+            // マスクのアルファ値取得（RGBA）
+            let maskColor = obj.mask.get(mouseX, mouseY);
+            let alpha = maskColor[3];
 
-function mouseDragged() {
-  for (let obj of images) {
-    if (obj.dragging) {
-      obj.pos = [mouseX - obj.offset[0], mouseY - obj.offset[1]];
-      redraw();
-    }
-  }
-}
+            // アルファが一定以上なら有効クリックと判定
+            if (alpha > 10) {
+              obj.dragging = true;
+              obj.offset = [mx, my];
+              break;
+            }
+          }
+        };
 
-function mouseReleased() {
-  for (let obj of images) {
-    obj.dragging = false;
-  }
-}
+        mouseDragged = () => {
+          for (let obj of images) {
+            if (obj.dragging) {
+              obj.pos = [mouseX - obj.offset[0], mouseY - obj.offset[1]];
+              redraw();
+            }
+          }
+        };
+
+        mouseReleased = () => {
+          for (let obj of images) {
+            obj.dragging = false;
+          }
+        };
+
+        /////////////////////////////////// ここまでp5.jsを貼り付け ///////////////////////////////////
